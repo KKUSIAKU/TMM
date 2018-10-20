38f906de0321937a02d41abbc459af9778a63cae
kkusiaku <kusiaku.k@gmail.com>
2018-10-20 15:48:20 +0200

    start layer class component

--- material.cpp
+++ material.cpp
@@ -15,15 +15,29 @@ class Material{
 		Material(string name, const complex<double>& index):_name(name),_index(index){}
 		~Material(){}
 		
-		void setIndex(complex<double>& index){
+		void setIndex(const complex<double>& index){
 				if(_index !=  complex <double> (0,0)){
 				_index = index;
 				
 				};
 			}
+			
+		 auto getIndex() const {
+			 return _index;
+			}
 
 };
 
+
+class Layer:public Material{
+		private:
+			double _thickness =0;
+	
+		public:
+			Layer(string name, const complex<double>& index):Material(name,index){}
+			Layer(string name, const complex<double>& index, double thickness):Material(name,index),_thickness(thickness){}
+	
+	};
 
 
 int main(){
